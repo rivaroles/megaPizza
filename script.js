b83a3a5111ca96d2f42f8ea68833d43de83c8fcb
Riva <rivaroles98@gmail.com>
2022-09-30 23:52:55 -0400

    megapizza setup

---
+++ script.js
@@ -0,0 +1,27 @@
+const pesquisa = document.querySelector("#pesquisa")
+//fetch usa crase (`)
+/*
+function changeState() { 
+  let h3 = document.querySelector('.selected')
+  
+  h3.classList.add('selected')
+
+  console.log(h3)
+  
+  // de achar o elemento que foi clicado
+  // achar todos os elementos h3
+  // rodar a funcao, e colocar a classe selected no elemento clicado
+  // continuacao da funcao
+}
+
+changeState()
+
+// const h3s = document.querySelectorAll('h3') // isso daqui encontra todos os h3s
+
+document.addEventListener('click', changeState())
+
+
+// document.addEventListener('click',)
+*/
+
+
